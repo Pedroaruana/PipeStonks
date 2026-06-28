89f4de90b2e884008733ae8bb80ccf3d0b914977
PEDRO <aruanapedro@gmail.com>
2026-06-28 10:40:15 -0300

    fix tipos vite env e import de assets
    resolve erros de tsc no build pra preparar a CI

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+
+declare module '*.jpg'
+declare module '*.png'
+declare module '*.svg'
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
